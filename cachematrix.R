a54737a58403c97d8324906873a5eaf7df5ec910
Allan Costa <allaninocencio@yahoo.com.br>
2014-07-27 01:12:43 -0300

    Add usage example.

--- cachematrix.R
+++ cachematrix.R
@@ -1,4 +1,23 @@
-## These functions aim to create a cached matrix inversion function
+## These functions aim to create a cached matrix inversion function.
+## Usage example:
+## > x <- matrix(c(1,3,2,5,6,7,9,10,11), 3 , 3)
+## > x
+##      [,1] [,2] [,3]
+## [1,]    1    5    9
+## [2,]    3    6   10
+## [3,]    2    7   11
+## > x_cache <- makeCacheMatrix(x)
+## > cacheSolve(x_cache) # It's the first calling, calculate it
+##            [,1]       [,2]       [,3]
+## [1,] -0.3333333  0.6666667 -0.3333333
+## [2,] -1.0833333 -0.5833333  1.4166667
+## [3,]  0.7500000  0.2500000 -0.7500000
+## > cacheSolve(x_cache) # It's already cached, just return it
+## getting cached data
+##            [,1]       [,2]       [,3]
+## [1,] -0.3333333  0.6666667 -0.3333333
+## [2,] -1.0833333 -0.5833333  1.4166667
+## [3,]  0.7500000  0.2500000 -0.7500000
 
 ## Create a list with special functions to cache an inverted matrix
 
